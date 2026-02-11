2207eb1f1e8c181f25e7643029aae71f17695108
omomo <om062006@gmail.com>
2026-02-12 02:08:39 +0530

    add chip struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+struct Chip {
+    mem: [u8; 4096], //4kB ram
+    rv: [u8; 16], //16 8-bit v registers
+    ri: u16, //one 16 bit register
+    dt: u8, //delay timer
+    st: u8, //sound timer
+    pc: u16, //program counter
+    sp: u8, //stack pointer
+    stack: [u16; 16],
+    keyboard: [bool; 16], //16 keys, either pressed or not pressed
+    display: [bool; 64*32], //pixels either on or off
+}
+
+
+
 fn main() {
-    println!("Hello, world!");
+
 }
